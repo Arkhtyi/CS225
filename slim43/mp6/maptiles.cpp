268da13c139f90ca69b10ae84a3e66bfea7153cd
slim43 <slim43@41665687-257a-41c1-8cf5-a96838db8e14>
2015-11-17 23:23:48 +0000

    done

--- slim43/mp6/maptiles.cpp
+++ slim43/mp6/maptiles.cpp
@@ -5,6 +5,7 @@
 	 			
 #include <iostream>
 #include <map>
+#include <vector>
 #include "maptiles.h"
 
 using namespace std;
@@ -16,49 +17,56 @@ MosaicCanvas * mapTiles(SourceImage const & theSource, vector<TileImage> const &
      */
   
     MosaicCanvas* canvas = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
-   /* 
-    KDTree theTree;
-    Point<Dim> rightPoint;
-    Point<Dim> SourceAverage;
-    vector < Point<Dim> > TilesRGBA;
+   
+    
+    Point<3> rightPoint;
+    Point<3> SourceAverage;
+    vector < Point<3> > TilesRGBA;
     RGBAPixel sourceColor;
     RGBAPixel tileColor;
     RGBAPixel rightColor;
-    int	tileNum;
+    int	tileNum = 0;
+    
+    
+    TilesRGBA.resize(theTiles.size());
+    for(size_t z = 0; z < theTiles.size(); z++){
+    			
+    			tileColor =theTiles[z].getAverageColor();
+    			
+				TilesRGBA[z][0] = tileColor.red;
+				TilesRGBA[z][1] = tileColor.green;
+				TilesRGBA[z][2] = tileColor.blue;
+				
+				
+    		}
+    KDTree<3> theTree(TilesRGBA);
+    
     
     for(int i = 0; i < theSource.getColumns(); i++){
     	for(int j = 0; j < theSource.getRows(); j++){
     		sourceColor = theSource.getRegionColor(j,i);
-    		TilesRGBA.resize(theTiles.size());
+    		
     		
     		SourceAverage[0] = sourceColor.red;
     		SourceAverage[1] = sourceColor.green;
 			SourceAverage[2] = sourceColor.blue;
 				
     		
-    		for(size_t z = 0; z < theTiles.size(); z++){
-    			
-    			tileColor =theTiles[z].getAverageColor();
-    			
-				Tiles.RGBA[z][0] = tileColor.red;
-				Tiles.RGBA[z][1] = tileColor.green;
-				Tiles.RGBA[z][2] = tileColor.blue;
-				
-				
+    		
+    		rightPoint = theTree.findNearestNeighbor(SourceAverage);
+    		
+    		for(int k = 0; k < (int)TilesRGBA.size(); k++){
+    			if((TilesRGBA[k][0] == rightPoint[0]) && (TilesRGBA[k][1] == rightPoint[1]) && (TilesRGBA[k][2] == rightPoint[2]))
+    				tileNum = k;
     		}
-    		theTree = theTree(TilesRGBA);
-    		rightPoints = theTree.findNearestNeighbor(SourceAverage);
-    		
-    		rightColor.red = rightPoints[0];
-    		rightColor.green = rightPoints[1];
-    		rightColor.blue = rightPoints[1];
     		
     		
-    		canvas->setTile(j, i, rightColor);
+    		
+    		canvas->setTile(j, i, theTiles[tileNum]);
     		
     	}
     }
-    */
+    
      
 	return canvas;
 }
