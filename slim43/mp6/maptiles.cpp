13dedc6550a18f4c344cd5f1a2296a5e42e869e5
slim43 <slim43@41665687-257a-41c1-8cf5-a96838db8e14>
2015-11-17 03:21:15 +0000

    argh

--- slim43/mp6/maptiles.cpp
+++ slim43/mp6/maptiles.cpp
@@ -14,5 +14,22 @@ MosaicCanvas * mapTiles(SourceImage const & theSource, vector<TileImage> const &
     /**
      * @todo Implement this function!
      */
+  /*
+    MosaicCanvas* canvas = new MosaicCanvs(theSource.getRows(), theSource.getColumns());
+    
+    RGBAPixel sourceColor;
+    RGBAPixel tileColor;
+    
+    for(int i = 0; i < theSource.getColumns(); i++){
+    	for(int j = 0; j < theSource.getRows(); j++){
+    		sourceColor = theSource.getRegionColor(j,i);\
+    		for(int z = 0; z < theTiles.size(); z++){
+    			
+    		}
+    		
+    	}
+    }
+    */
+     
 	return NULL;
 }
