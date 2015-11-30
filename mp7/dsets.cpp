1d9ddb82cfbc5e65b5823f1fec2cfe60f5c57b2d
slim43 <slim43@41665687-257a-41c1-8cf5-a96838db8e14>
2015-11-30 01:56:44 +0000

    7.1 done

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -1,1 +1,37 @@
 /* Your code here! */
+
+#include "dsets.h"
+
+void DisjointSets::addelements (int  num){
+	
+	
+	for(int i = 0; i < num; i++){
+		joints.push_back(-1);
+	}
+	
+
+}
+
+int DisjointSets::find(int elem){
+
+	if(joints[elem] < 0) return elem;
+	else return find(joints[elem]);
+
+}
+
+void DisjointSets::setunion(int a, int b){
+	int roota = find(a);
+	int rootb = find(b);
+	int newsize = joints[roota] + joints[rootb];
+	if(roota > rootb){
+		joints[rootb] = roota;
+		joints[roota] = newsize;
+	}
+	else{
+		joints[roota] = rootb;
+		joints[rootb] = newsize;
+	}
+
+}
+
+
--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,24 @@
 /* Your code here! */
+
+#ifndef dsets_h
+#define dsets_h
+
+#include <vector>
+
+class DisjointSets {
+
+private:
+	
+	std::vector<int> joints;
+
+public:
+
+	void addelements (int num) ;
+
+	int find(int elem);
+	
+	void setunion(int a, int b);
+
+};
+
+#endif
